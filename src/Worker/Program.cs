9b1634dfed02440144428b31f55fa9ea36480655
Marcin <miazgamarcin@outlook.com>
2023-10-04 22:03:01 +0200

    fix naming of the Worker in Worker

--- src/Worker/Program.cs
+++ src/Worker/Program.cs
@@ -3,7 +3,7 @@ using Worker;
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
-        services.AddHostedService<Worker>();
+        services.AddHostedService<BackgroundWorker>();
     })
     .Build();
 
